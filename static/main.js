70b9dfbd95748b5389949987d3577626fe7cb951
martin <Martin.yu@baiyitech.com.tw>
2023-08-25 15:11:38 +0800

    no message

--- static/main.js
+++ static/main.js
@@ -23,12 +23,10 @@ router.afterEach((to, from, next) => {
   window.scrollTo(0, 0);
 });
 
-
-
-Vue.directive('image-loaded', {
+Vue.directive("image-loaded", {
   bind(el, binding, vnode) {
     const callback = binding.value;
-    const images = el.getElementsByTagName('img');
+    const images = el.getElementsByTagName("img");
     let loadedImageCount = 0;
 
     const onImageLoaded = () => {
@@ -40,7 +38,7 @@ Vue.directive('image-loaded', {
     };
 
     Array.from(images).forEach((image) => {
-      image.addEventListener('load', onImageLoaded);
+      image.addEventListener("load", onImageLoaded);
     });
   },
 });
@@ -54,85 +52,21 @@ new Vue({
     "header-component": headerComponent,
     "footer-component": footerComponent,
     "bottom-component": bottomComponent,
+    "download-dialog-component": downloadDialogComponent,
   },
 
   data() {
-    return {
-      downloadDialog: false,
-      qr: null,
-    };
+    return {};
   },
   computed: {
     isMobile() {
       return breakpoint.mobile;
     },
-    operatingSystem() {
-      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-      // 判斷iOS
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "iOS";
-      }
-      // 判斷Android
-      if (/android/i.test(userAgent)) {
-        return "Android";
-      }
-      return "unknown";
-    },
-
-    btnOptions() {
-      return [
-        {
-          icon: "./static/img/home/icon_qlb.png",
-          i18nKey: "home.menu.str3",
-          event: () => {
-            const link = document.createElement('a');
-            link.href = 'webclip/install.html';
-            link.target = '_blank';
-            link.click();
-          },
-          bgColor: "#867cb9",
-          textColor: "#33286f",
-          key: "key1",
-          isShow: this.operatingSystem == "unknown" || this.operatingSystem == "iOS",
-        },
-        {
-          icon: "./static/img/home/icon_ptxz.png",
-          i18nKey: "home.menu.str2",
-          event: () => {
-            // openLink(apk);
-          },
-          bgColor: "#c9a17e",
-          textColor: "#563518",
-          key: "key2",
-          isShow: this.operatingSystem == "unknown" || this.operatingSystem == "iOS",
-        },
-        {
-          icon: "./static/img/home/icon_az1.png",
-          i18nKey: "home.menu.str1",
-          event: () => {
-            openLink(apk);
-          },
-          bgColor: "#74b654",
-          textColor: "#295514",
-          key: "key3",
-          isShow: this.operatingSystem == "unknown" || this.operatingSystem == "Android",
-        },
-        // {
-        //   icon: "./static/img/home/icon_ewm.png",
-        //   i18nKey: "home.menu.str4",
-        //   event: () => {},
-        //   bgColor: "#c8cbd4",
-        //   textColor: "",
-        //   key: "key4",
-        //   isShow: !this.isMobile,
-        // },
-      ];
-    },
   },
   methods: {
     showDownloadDialog() {
-      this.downloadDialog = true;
-      this.qr = qrImg;
+      this.$refs["downloadDialog"].downloadDialog = true;
+      this.$refs["downloadDialog"].qr = qrImg;
     },
   },
 }).$mount("#app");
